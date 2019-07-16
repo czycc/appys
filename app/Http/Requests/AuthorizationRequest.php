93617850f493904618a1ff5fb5792680ad674fed
zhaoyang <602155776@qq.com>
2019-07-16 17:25:02 +0800

    已注册用户绑定微信

--- app/Http/Requests/AuthorizationRequest.php
+++ app/Http/Requests/AuthorizationRequest.php
@@ -17,7 +17,8 @@ class AuthorizationRequest extends Request
                 'required',
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
             ],
-            'password' => 'required|string|min:6'
+            'password' => 'required|string|min:6',
+            'wx_id' => 'string'
         ];
     }
 
