565cf95e638976d77097add9901a435909e6b5e9
zhaoyang <602155776@qq.com>
2019-07-18 15:00:45 +0800

    发送验证码接口

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ $api->version('v1', [
 
     $api->group([
         'middleware' => 'api.throttle',
-        'limit' => 1,
+        'limit' => 2,
         'expires' => 1
     ], function ($api) {
         //发送验证码
