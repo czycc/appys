0a1933b59fc3b13aef1ac04c272bd8310e8b69fa
zhaoyang <602155776@qq.com>
2019-07-21 22:24:29 +0800

    查询指定用户店铺下的文章列表

--- app/Http/Controllers/Api/v1/ArticlesController.php
+++ app/Http/Controllers/Api/v1/ArticlesController.php
@@ -15,18 +15,23 @@ class ArticlesController extends Controller
     {
     }
 
-	public function index(Request $request, Article $article)
-	{
+    public function index(Request $request, Article $article)
+    {
         $query = $article->query();
-	    if (!is_null($request->input('status'))) {
-	        //按状态查询，只针对当前用户
+
+        if (!is_null($request->input('status'))) {
+            //按状态查询，只针对当前用户
             $query->where('user_id', $this->user()->id)
                 ->where('status', $request->input('status'));
-	    } else {
+        } else {
             //查询审核痛过
             $query->where('status', 1);
         }
 
+        if ($user_id = $request->user_id) {
+            //查询指定用户店铺下通过审核的文章
+            $query->where('user_id', $user_id)->where('status', 1)
+        }
         if ($request->type) {
             //查询指定类型
             $query->where('type', $request->type);
@@ -36,7 +41,7 @@ class ArticlesController extends Controller
 
         return $this->response->paginator($posts, new ArticleTransformer(true));
 
-	}
+    }
 
     public function show(Article $article)
     {
@@ -44,38 +49,38 @@ class ArticlesController extends Controller
     }
 
 
-	public function store(ArticleRequest $request, Article $article)
-	{
-	    $article->fill($request->all());
-	    $article->user_id = $this->user()->id;
-	    $article->status = 2; //避免transformer返回null
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $this->user()->id;
+        $article->status = 2; //避免transformer返回null
         $article->top_img = json_decode($request->multi_imgs)[0];//第一张作为头图
 
         //根据后台设置价格
         $price = ArticlePrice::find($request->price_id);
         $article->price = $price->price;
-	    $article->save();
+        $article->save();
 
-	    if ($request->tags) {
-	        $article->tags()->attach(json_decode($request->tags));
+        if ($request->tags) {
+            $article->tags()->attach(json_decode($request->tags));
         }
-	    return $this->response
+        return $this->response
             ->item($article, new ArticleTransformer())
             ->setStatusCode(201);
-	}
+    }
 
-	public function update(ArticleRequest $request, Article $article)
-	{
-		$this->authorize('update', $article);
-		$article->update($request->all());
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $this->authorize('update', $article);
+        $article->update($request->all());
 
-		return $this->response->item($article, new ArticleTransformer());
-	}
+        return $this->response->item($article, new ArticleTransformer());
+    }
 
-	public function destroy(Article $article)
-	{
-		$this->authorize('destroy', $article);
-		$article->delete();
+    public function destroy(Article $article)
+    {
+        $this->authorize('destroy', $article);
+        $article->delete();
         return $this->response->noContent();
-	}
+    }
 }
