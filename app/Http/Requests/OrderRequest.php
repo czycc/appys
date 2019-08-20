a9b9b313493088ebe16193e9d8ed614b440294fa
zhaoyang <602155776@qq.com>
2019-08-20 13:39:21 +0800

    优化

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -14,7 +14,7 @@ class OrderRequest extends Request
     {
         $rules = [
             'type' => 'required|in:chapter,article,course,vip',
-            'pay_method' => 'required|in:alipay,wechat',
+            'pay_method' => 'required|in:alipay,wechat,wap',
         ];
 
         if ($this->type == 'chapter') {
