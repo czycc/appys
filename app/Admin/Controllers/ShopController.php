0c8ab0e421542db00e59a5af7ff415f8247f65c4
zhaoyang <602155776@qq.com>
2019-08-23 18:35:45 +0800

    店铺

--- app/Admin/Controllers/ShopController.php
+++ app/Admin/Controllers/ShopController.php
@@ -135,6 +135,40 @@ class ShopController extends AdminController
     {
         $form = new Form(new Shop);
 
+        $form->footer(function ($footer) {
+
+            // 去掉`重置`按钮
+//            $footer->disableReset();
+
+            // 去掉`提交`按钮
+//            $footer->disableSubmit();
+
+            // 去掉`查看`checkbox
+            $footer->disableViewCheck();
+
+            // 去掉`继续编辑`checkbox
+            $footer->disableEditingCheck();
+
+            // 去掉`继续创建`checkbox
+            $footer->disableCreatingCheck();
+
+        });
+
+        $form->tools(function (Form\Tools $tools) {
+
+            // 去掉`列表`按钮
+//            $tools->disableList();
+
+            // 去掉`删除`按钮
+            $tools->disableDelete();
+
+            // 去掉`查看`按钮
+            $tools->disableView();
+
+            // 添加一个按钮, 参数可以是字符串, 或者实现了Renderable或Htmlable接口的对象实例
+//            $tools->add('<a class="btn btn-sm btn-danger"><i class="fa fa-trash"></i>&nbsp;&nbsp;delete</a>');
+        });
+
 //        $form->textarea('introduction', __('Introduction'));
 //        $form->text('banner', __('Banner'));
 //        $form->text('idcard', __('Idcard'));
--- app/Observers/ShopObserver.php
+++ app/Observers/ShopObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Shop;
+use App\Notifications\NormalNotify;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -19,6 +20,18 @@ class ShopObserver
         if (is_null($shop->expire_at)) {
             //店铺过期时间和用户一致
             $shop->expire_at = $shop->user->expire_at;
+        }
+
+        if ($shop->isDirty('recommend') && $shop->recommend) {
+            $shop->user->msgNotify(new NormalNotify('店铺被设为推荐', '您的店铺已被设为推荐', 'shop', $shop->id));
+        }
+
+        if ($shop->isDirty('status')) {
+            if ($shop->status === 0) {
+                $shop->user->msgNotify(new NormalNotify('店铺未通过认证', '您的店铺未通过认证，请及时修改', 'shop', $shop->id));
+            } elseif ($shop->status === 1) {
+                $shop->user->msgNotify(new NormalNotify('店铺已通过认证', '恭喜，您的店铺已经通过认证。', 'shop', $shop->id));
+            }
         }
     }
 }
