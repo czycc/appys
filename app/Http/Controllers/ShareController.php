0e6a7c8fee78b055cd9ff40363f7bb8740f230b7
zhaoyang <602155776@qq.com>
2019-08-28 18:00:23 +0800

    分享地址修改

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -21,7 +21,12 @@ class ShareController extends Controller
                 $url = 'http://api.woheni99.com/UserArticleDetail?id=' . $id;
                 break;
             case 'company_post':
-                $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d2b37e7c2938683&redirect_uri=http://api.woheni99.com/CompanyArticleDetail?id={$id}&response_type=code&scope=snsapi_base&state=share#wechat_redirect";
+                if ($user = User::find($user_id)) {
+                    $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d2b37e7c2938683&redirect_uri=http://api.woheni99.com/CompanyArticleDetail?id={$id}&{$user->phone}&response_type=code&scope=snsapi_base&state=share";
+                } else {
+                    $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5d2b37e7c2938683&redirect_uri=http://api.woheni99.com/CompanyArticleDetail?id={$id}&response_type=code&scope=snsapi_base&state=share";
+                }
+                return redirect($url);
                 break;
             case 'shop':
                 $url = 'http://api.woheni99.com/Shop?id=' . $id;
