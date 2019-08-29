6a49dae75affb2a2c9f93d698d50d8b438cea774
zhaoyang <602155776@qq.com>
2019-08-29 09:51:23 +0800

    上下级

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -224,11 +224,11 @@ class UsersController extends Controller
         //不能循环上下级
         if ($user->bound_id) {
             if ($user->bound_id === $this->user()->id) {
-                return $this->response->errorBadRequest('不能循环绑定上下级');
+                return $this->response->errorBadRequest('不能循环绑定自己的下级');
             } else {
                 if ($top = User::find($user->bound_id)) {
                     if ($top->bound_id === $this->user()->id) {
-                        return $this->response->errorBadRequest('不能循环绑定上下级');
+                        return $this->response->errorBadRequest('不能循环绑定自己的下级');
                     }
                 }
             }
