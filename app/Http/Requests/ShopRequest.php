89648b47bef21d4c69d2d107050d30917d62591f
zhaoyang <602155776@qq.com>
2019-09-12 11:13:57 +0800

    手机号码正则验证问题

--- app/Http/Requests/ShopRequest.php
+++ app/Http/Requests/ShopRequest.php
@@ -13,7 +13,7 @@ class ShopRequest extends Request
                     return [
                         'shop_phone' => [
                             'required',
-                            'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
+                            'regex:/^(1)\d{10}$/',
                         ],
                         'real_name' => 'required|string|min:2|max:5',
                         'banner' => 'required|string',
--- app/Http/Requests/VerificationCodeRequest.php
+++ app/Http/Requests/VerificationCodeRequest.php
@@ -16,7 +16,7 @@ class VerificationCodeRequest extends Request
             return [
                 'phone' => [
                     'required',
-                    'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
+                    'regex:/^(1)\d{10}$/',
                     'unique:users'
                 ]
             ];
@@ -24,7 +24,7 @@ class VerificationCodeRequest extends Request
             return [
                 'phone' => [
                     'required',
-                    'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
+                    'regex:/^(1)\d{10}$/',
                     'exists:users,phone'
                 ]
             ];
