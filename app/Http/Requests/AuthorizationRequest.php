54605797fc4697c5bde2dd7939082f2b0ea26c07
zhaoyang <602155776@qq.com>
2019-09-12 11:10:15 +0800

    手机号码正则验证问题

--- app/Http/Requests/AuthorizationRequest.php
+++ app/Http/Requests/AuthorizationRequest.php
@@ -15,7 +15,7 @@ class AuthorizationRequest extends Request
         return [
             'phone' => [
                 'required_without:verify_key,verify_code',
-                'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
+                'regex:/^(1)\d{10}$/',
             ],
             'password' => 'required_without:verify_key,verify_code|string|min:6',
             'verify_key' => 'string',
