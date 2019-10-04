16139d2d60c907aa12d94009fd8682153ca3f590
zhaoyang <602155776@qq.com>
2019-10-04 18:20:13 +0800

    用户微信提现

--- app/Models/FlowOut.php
+++ app/Models/FlowOut.php
@@ -8,9 +8,9 @@ use Pay;
 
 class FlowOut extends Model
 {
-    protected $fillable =['total_amount', 'out_method', 'ali_account'];
+    protected $fillable = ['total_amount', 'out_method', 'ali_account'];
 
-    public $casts =[
+    public $casts = [
         'status' => 'boolean',
         'out_status' => 'boolean',
         'is_offline' => 'boolean',
@@ -22,7 +22,8 @@ class FlowOut extends Model
         return $this->belongsTo(User::class);
     }
 
-    public static function boot() {
+    public static function boot()
+    {
 
         parent::boot();
 
@@ -39,6 +40,12 @@ class FlowOut extends Model
                 if ($model->out_method === 'wechat') {
                     Pay::wechat()->transfer($model->out_info);
                     $model->out_status = 1;
+                    $user = User::find($model->user_id);
+                    $user->msgNotify(new NormalNotify(
+                        '提现成功',
+                        '您已成功提现' . $model->total_amount,
+                        'flow_out'
+                    ));
                 }
 
             }
--- app/Transformers/FlowOutTransformer.php
+++ app/Transformers/FlowOutTransformer.php
@@ -11,11 +11,16 @@ class FlowOutTransformer extends TransformerAbstract
 
     public function transform(FlowOut $item)
     {
+        $out_status = $item->out_status;
+
+        if ($out_status != 1 && $item->is_offline == 1) {
+            $out_status = true;
+        }
         return [
             'id' => $item->id,
             'total_amount' => $item->total_amount,
             'status' => $item->status,
-            'out_status' => $item->out_status,
+            'out_status' => $out_status,
             'created_at' => (string)$item->created_at,
 
         ];
